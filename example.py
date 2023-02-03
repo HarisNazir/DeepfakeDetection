ba4b35c3da6988349f50344b741cf27a00c59b25
Haris Nazir <haris.nazir1963@gmail.com>
2023-02-03 16:34:16 +0000

    Added accuracy check

--- example.py
+++ example.py
@@ -1,9 +1,12 @@
 import numpy as np
 from classifiers import *
 from pipeline import *
+import json
 
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
+with open('data/metadata.json') as f:
+    ground_truth = json.load(f)
 # 1 - Load the model and its pretrained weights
 classifier = Meso4()
 classifier.load('weights/Meso4_DF.h5')
@@ -14,11 +17,11 @@ classifier.load('weights/Meso4_DF.h5')
 
 dataGenerator = ImageDataGenerator(rescale=1./255)
 generator = dataGenerator.flow_from_directory(
-        'test_images',
-        target_size=(256, 256),
-        batch_size=1,
-        class_mode='binary',
-        subset='training')
+    'test_images',
+    target_size=(256, 256),
+    batch_size=1,
+    class_mode='binary',
+    subset='training')
 
 # 3 - Predict
 X, y = generator.next()
@@ -30,4 +33,17 @@ classifier.load('weights/Meso4_F2F.h5')
 
 predictions = compute_accuracy(classifier, 'data')
 for video_name in predictions:
-    print('`{}` video class prediction :'.format(video_name), predictions[video_name][0])
+    print('`{}` video class prediction :'.format(
+        video_name), predictions[video_name][0])
+
+correct_predictions = 0
+total_predictions = len(predictions)
+
+for video_name, prediction in predictions.items():
+    prediction_value = prediction[0]
+    ground_truth_value = ground_truth[video_name]['label']
+
+    if prediction_value == ground_truth_value:
+        correct_predictions += 1
+accuracy = correct_predictions / total_predictions
+print('FINAL ACCURACY: ', accuracy)
