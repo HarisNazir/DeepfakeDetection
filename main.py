8d4c7ff9e487d924f8201f151bf18b52f79c5da2
Haris Nazir <haris.nazir1963@gmail.com>
2023-02-08 15:22:03 +0000

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
 from keras.models import Sequential
 from keras.layers import Dense, LSTM
+from keras.applications.resnet50 import ResNet50
 
 # Load the metadata
 with open('data/metadata.json', 'r') as f:
@@ -21,58 +22,62 @@ test_files = [k for k, v in metadata.items() if v['split'] == 'test']
 
 
 def preprocess(video_file):
-    # TODO: Preprocess the video file and return the features
-    # For example, you can extract the frames from the video and convert them into a numpy array
+    # Split the video into frames
+    frames = split_into_frames(video_file)
+    
+    # Detect faces in the frames
+    faces = detect_faces(frames)
+    
+    # Remove frames without faces
+    frames = [f for f in frames if f is not None]
+    
+    # Crop the first 300 frames from each video
+    frames = frames[:300]
+    
+    # Convert frames to numpy array
+    X = np.array(frames)
+    
+    # Use ResNeXt-50 CNN to extract features
+    X = extract_features(X)
+    
+    return X
+
+def split_into_frames(video_file):
+    cap = cv2.VideoCapture(video_file)
+    frames = []
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        frames.append(frame)
+    cap.release()
+    return frames
+
+def detect_faces(frames):
+    face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+    faces = []
+    for frame in frames:
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        face = face_cascade.detectMultiScale(gray, 1.3, 5)
+        faces.append(face)
+    return faces
+
+
+def extract_features(preprocessed_data):
+    # Create an instance of the ResNeXt50 model
+    resnext50_model = ResNet50(weights='imagenet', include_top=False, input_shape=(224,224,3))
+    
+    # Extract features from the preprocessed data using the ResNeXt50 model
+    features = resnext50_model.predict(preprocessed_data)
+    
+    # Flatten the extracted features
+    features = features.reshape(features.shape[0], -1)
+    
     return features
 
-
-X_train = np.array([preprocess(os.path.join('data', f)) for f in train_files])
-y_train = np.array([metadata[f]['label'] for f in train_files])
-X_test = np.array([preprocess(os.path.join('data', f)) for f in test_files])
-y_test = np.array([metadata[f]['label'] for f in test_files])
-
-# One hot encoding of labels
-labels = np.unique(y_train)
-one_hot_map = {l: i for i, l in enumerate(labels)}
-y_train = np.array([one_hot_map[y] for y in y_train])
-y_test = np.array([one_hot_map[y] for y in y_test])
-
-# Build the model
-model = Sequential()
-model.add(LSTM(32, input_shape=(X_train.shape[1], X_train.shape[2])))
-model.add(Dense(len(labels), activation='softmax'))
-model.compile(loss='categorical_crossentropy',
-              optimizer='adam', metrics=['accuracy'])
-
-# Train the model
-history = model.fit(X_train, y_train, batch_size=32,
-                    epochs=10, validation_data=(X_test, y_test))
-
-# Plot the accuracy and loss
-plt.plot(history.history['accuracy'])
-plt.plot(history.history['val_accuracy'])
-plt.title('Model accuracy')
-plt.ylabel('Accuracy')
-plt.xlabel('Epoch')
-plt.legend(['Train', 'Test'], loc='upper left')
-plt.show()
-
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.title('Model loss')
-plt.ylabel('Loss')
-plt.xlabel('Epoch')
-plt.legend(['Train', 'Test'], loc='upper left')
-plt.show()
-
-# Plot the confusion matrix
-y_pred = model.predict(X_test)
-y_pred = np.argmax(y_pred, axis=1)
-y_true = np.argmax(y_test, axis=1)
-cm = confusion_matrix(y_true, y_pred)
-
-df_cm = pd.DataFrame(cm, index=[i for i in ["REAL", "FAKE"]], columns=[i for i in ["REAL", "FAKE"]])
-plt.figure(figsize=(10, 7))
-plt.title("Confusion Matrix")
-sn.heatmap(df_cm, annot=True)
-plt.show()
+def create_model():
+    model = Sequential()
+    model.add(LSTM(128, input_shape=(300, num_features)))
+    model.add(Dense(1, activation='sigmoid'))
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
